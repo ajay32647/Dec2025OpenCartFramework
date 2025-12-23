03ba04b83fca20dd239db8484946b89ddc24f005
Ajay Srivastava <ajaysrivastava@Ajays-MacBook-Air.local>
2025-12-23 15:11:34 +0530

    updated code

--- src/main/java/com/qa/opencart/pages/AddtoCart.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-public class AddtoCart {
-	int j = 10;
-
-	public void addtoCart() {
-
-	}
-}
--- src/main/java/com/qa/opencart/pages/PaymentPage.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-public class PaymentPage {
-	int i = 10;
-
-	public void paymentPage() {
-
-	}
-}
